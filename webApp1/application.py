3b1c9c82dc1d9a2cccbc88ff6d0d07c20bc5e909
Katrina E. Wheelan <kw8@williams.edu>
2020-06-28 22:33:20 -0400

    added a few pages

--- webApp1/application.py
+++ webApp1/application.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 
 app.static_folder = 'static'
 
-#API_KEY = "AIzaSyB_TMYCnCqQz_UDRc6wsu7Tw7rMUbgQ0hQ"
+#api key for google civic api
+API_KEY = "AIzaSyB_TMYCnCqQz_UDRc6wsu7Tw7rMUbgQ0hQ"
 
 @app.route("/")
 def index():
@@ -48,3 +49,48 @@ def registration():
 
     text = get_registration("https://verify.vote.org/", data)
     return render_template("registration.html", text=text)
+
+@app.route("/contact")
+def contact():
+    """ home page """
+    return render_template("contact.html")
+
+@app.route("/faqs")
+def faqs():
+    return "In progress"
+
+@app.route("/poll_finder")
+def pollFinder():
+    """Get polling info."""
+
+    # Get form information.
+    line1 = request.form.get("address")
+    city = request.form.get("city")
+    state = request.form.get("state")
+    zip = request.form.get("zip")
+
+    if city.lower()=="san francisco":
+        name, address, hours = get_poll_info("https://www.sfelections.org/tools/pollsite/", line1, zip)
+        return render_template("info.html", name=name, line1=address, hours=hours)
+
+    else:
+        address = "{} {} {} {}".format(line1, city, state, zip)
+
+        #HTTP request to API
+        res = requests.get("https://www.googleapis.com/civicinfo/v2/voterinfo",
+                           params={"address": address, "returnAllAvailableData": True,
+                           "key": API_KEY})
+        if res.status_code != 200:
+            return "ERROR: Badly formatted address."
+        data = res.json()
+        try:
+            location = data["pollingLocations"][0]["address"]["locationName"]
+            line1 = data["pollingLocations"][0]["address"]["line1"]
+        except:
+            location = "No polling data for this address or no upcoming elections."
+            line1 = ""
+        try:
+            hours = data["pollingLocations"][0]["pollingHours"]
+        except:
+            hours = "No available hours for this location"
+        return render_template("info.html", name=location, line1=line1, hours=hours)
