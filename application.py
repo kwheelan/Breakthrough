b1cef8e7914cfa7e2f10a7aefb068736876c41c0
Katrina Wheelan <kw8@williams.edu>
2020-07-09 13:13:34 -0400

    translations setup

--- application.py
+++ application.py
@@ -2,16 +2,84 @@ import os
 
 from flask import Flask, render_template, request, session
 import requests
-
 from formTools import *
 
 app = Flask(__name__)
 app.secret_key = '1k93khlj15jK'
-
 app.static_folder = 'static'
 
 #api key for google civic api
 API_KEY = "AIzaSyB_TMYCnCqQz_UDRc6wsu7Tw7rMUbgQ0hQ"
+
+#Helper functions
+def registrationHelper():
+    """Get registration information"""
+    data = {}
+
+    #Name
+    data["first_name"] = request.form.get("firstName")
+    data["last_name"] = request.form.get("lastName")
+    name = "{} {}".format(data["first_name"], data["last_name"])
+
+    # Get address information.
+    data["street_address"] = request.form.get("address")
+    ## TODO: enable apartments
+    data["apartment"] = ""
+    data["city"] = request.form.get("city")
+    data["state"] = request.form.get("state")
+    data["zip_5"] = request.form.get("zip")
+
+    address = "{}, {}, {} {}".format(data["street_address"], data["city"], data["state"], data["zip_5"])
+
+    for key in ["street_address", "apartment", "city", "state", "zip_5"]:
+        session[key] = data[key]
+
+    #dob
+    data["date_of_birth_month"] = 1#request.form.get("mon")
+    data["date_of_birth_day"] = 1#request.form.get("day")
+    data["date_of_birth_year"] = 2000#request.form.get("year")
+
+    text = get_registration("https://verify.vote.org/", data)
+    return text, name, address
+
+def pollFinderHelper():
+    """Helper method to get poll information"""
+
+    # Get form information.
+    if request.method == 'POST':
+        line1 = request.form.get("address")
+        city = request.form.get("city")
+        state = request.form.get("state")
+        zip = request.form.get("zip")
+    if request.method == 'GET':
+        line1 = session["street_address"]
+        city = session["city"]
+        state = session["state"]
+        zip = session["zip_5"]
+
+    if city.lower()=="san francisco":
+        location, line1, hours = get_poll_info("https://www.sfelections.org/tools/pollsite/", line1, zip)
+    else:
+        address = "{} {} {} {}".format(line1, city, state, zip)
+
+        #HTTP request to API
+        res = requests.get("https://www.googleapis.com/civicinfo/v2/voterinfo",
+                           params={"address": address, "returnAllAvailableData": True,
+                           "key": API_KEY})
+        if res.status_code != 200:
+            return "ERROR: Badly formatted address."
+        data = res.json()
+        try:
+            location = data["pollingLocations"][0]["address"]["locationName"]
+            line1 = data["pollingLocations"][0]["address"]["line1"]
+        except:
+            location = "No polling data for this address or no upcoming elections."
+            line1 = ""
+        try:
+            hours = data["pollingLocations"][0]["pollingHours"]
+        except:
+            hours = "No available hours for this location"
+    return location, line1, hours
 
 #National english
 @app.route("/registration/query")
@@ -22,33 +90,7 @@ def registration_forms():
 @app.route("/registration/info", methods=["POST"])
 def registration():
     """Get polling info."""
-
-    data = {}
-
-    #Name
-    data["first_name"] = request.form.get("firstName")
-    data["last_name"] = request.form.get("lastName")
-    name = "{} {}".format(data["first_name"], data["last_name"])
-
-    # Get address information.
-    data["street_address"] = request.form.get("address")
-    ## TODO: enable apartments
-    data["apartment"] = ""
-    data["city"] = request.form.get("city")
-    data["state"] = request.form.get("state")
-    data["zip_5"] = request.form.get("zip")
-
-    address = "{}, {}, {} {}".format(data["street_address"], data["city"], data["state"], data["zip_5"])
-
-    for key in ["street_address", "apartment", "city", "state", "zip_5"]:
-        session[key] = data[key]
-
-    #dob
-    data["date_of_birth_month"] = 1#request.form.get("mon")
-    data["date_of_birth_day"] = 1#request.form.get("day")
-    data["date_of_birth_year"] = 2000#request.form.get("year")
-
-    text = get_registration("https://verify.vote.org/", data)
+    text, name, address = registrationHelper()
     return render_template("national/english/registration.html", text=text, name=name, address=address)
 
 @app.route("/poll_finder/query")
@@ -59,44 +101,8 @@ def poll_forms():
 @app.route("/poll_finder/info", methods = ["POST", "GET"])
 def pollFinder():
     """Get polling info."""
-
-    # Get form information.
-    if request.method == 'POST':
-        line1 = request.form.get("address")
-        city = request.form.get("city")
-        state = request.form.get("state")
-        zip = request.form.get("zip")
-    if request.method == 'GET':
-        line1 = session["street_address"]
-        city = session["city"]
-        state = session["state"]
-        zip = session["zip_5"]
-
-    if city.lower()=="san francisco":
-        name, address, hours = get_poll_info("https://www.sfelections.org/tools/pollsite/", line1, zip)
-        return render_template("national/english/poll_info.html", name=name, line1=address, hours=hours)
-
-    else:
-        address = "{} {} {} {}".format(line1, city, state, zip)
-
-        #HTTP request to API
-        res = requests.get("https://www.googleapis.com/civicinfo/v2/voterinfo",
-                           params={"address": address, "returnAllAvailableData": True,
-                           "key": API_KEY})
-        if res.status_code != 200:
-            return "ERROR: Badly formatted address."
-        data = res.json()
-        try:
-            location = data["pollingLocations"][0]["address"]["locationName"]
-            line1 = data["pollingLocations"][0]["address"]["line1"]
-        except:
-            location = "No polling data for this address or no upcoming elections."
-            line1 = ""
-        try:
-            hours = data["pollingLocations"][0]["pollingHours"]
-        except:
-            hours = "No available hours for this location"
-        return render_template("national/english/poll_info.html", name=location, line1=line1, hours=hours)
+    location, line1, hours = pollFinderHelper()
+    return render_template("national/english/poll_info.html", name=location, line1=line1, hours=hours)
 
 #National chinese
 @app.route("/man/registration/query")
@@ -107,33 +113,7 @@ def registration_forms_mandarin():
 @app.route("/man/registration/info", methods=["POST"])
 def registration_mandarin():
     """Get polling info."""
-
-    data = {}
-
-    #Name
-    data["first_name"] = request.form.get("firstName")
-    data["last_name"] = request.form.get("lastName")
-    name = "{} {}".format(data["first_name"], data["last_name"])
-
-    # Get address information.
-    data["street_address"] = request.form.get("address")
-    ## TODO: enable apartments
-    data["apartment"] = ""
-    data["city"] = request.form.get("city")
-    data["state"] = request.form.get("state")
-    data["zip_5"] = request.form.get("zip")
-
-    address = "{}, {}, {} {}".format(data["street_address"], data["city"], data["state"], data["zip_5"])
-
-    for key in ["street_address", "apartment", "city", "state", "zip_5"]:
-        session[key] = data[key]
-
-    #dob
-    data["date_of_birth_month"] = 1#request.form.get("mon")
-    data["date_of_birth_day"] = 1#request.form.get("day")
-    data["date_of_birth_year"] = 2000#request.form.get("year")
-
-    text = get_registration("https://verify.vote.org/", data)
+    text, name, address = registrationHelper()
     return render_template("national/mandarin/registration.html", text=text, name=name, address=address)
 
 @app.route("/man/poll_finder/query")
@@ -144,45 +124,8 @@ def poll_forms_mandarin():
 @app.route("/man/poll_finder/info", methods = ["POST", "GET"])
 def pollFinder_mandarin():
     """Get polling info."""
-
-    # Get form information.
-    if request.method == 'POST':
-        line1 = request.form.get("address")
-        city = request.form.get("city")
-        state = request.form.get("state")
-        zip = request.form.get("zip")
-    if request.method == 'GET':
-        line1 = session["street_address"]
-        city = session["city"]
-        state = session["state"]
-        zip = session["zip_5"]
-
-    if city.lower()=="san francisco":
-        name, address, hours = get_poll_info("https://www.sfelections.org/tools/pollsite/", line1, zip)
-        return render_template("national/mandarin/poll_info.html", name=name, line1=address, hours=hours)
-
-    else:
-        address = "{} {} {} {}".format(line1, city, state, zip)
-
-        #HTTP request to API
-        res = requests.get("https://www.googleapis.com/civicinfo/v2/voterinfo",
-                           params={"address": address, "returnAllAvailableData": True,
-                           "key": API_KEY})
-        if res.status_code != 200:
-            return "ERROR: Badly formatted address."
-        data = res.json()
-        try:
-            location = data["pollingLocations"][0]["address"]["locationName"]
-            line1 = data["pollingLocations"][0]["address"]["line1"]
-        except:
-            location = "No polling data for this address or no upcoming elections."
-            line1 = ""
-        try:
-            hours = data["pollingLocations"][0]["pollingHours"]
-        except:
-            hours = "No available hours for this location"
-        return render_template("national/mandarin/poll_info.html", name=location, line1=line1, hours=hours)
-
+    location, line1, hours = pollFinderHelper()
+    return render_template("national/mandarin/poll_info.html", name=location, line1=line1, hours=hours)
 
 #CA english
 @app.route("/")
@@ -210,3 +153,16 @@ def register_mandarin():
 @app.route("/man/ca/faqs")
 def faqs_mandarin():
     return render_template("CA/mandarin/faqs.html")
+
+#CA spanish
+@app.route("/sp")
+def CA_home_spanish():
+    return render_template("CA/spanish/CA_home_mandarin.html")
+
+@app.route("/sp/ca/register")
+def register_spanish():
+    return render_template("CA/spanish/register.html")
+
+@app.route("/sp/ca/faqs")
+def faqs_spanish():
+    return render_template("CA/spanish/faqs.html")
